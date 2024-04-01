02beac1748ec8097ba56367ce656252a5b6efd0e
Shahroz Ashraf khan <mr.sherry.793@gmail.com>
2024-04-01 10:46:26 +0500

    perform crud operation in express with pure javascript

--- index.js
+++ index.js
@@ -1,63 +1,103 @@
-// const lib=require('./lib.js') //  for common js CJS
-// // import {sum,diff} from './lib.js'   for ESM
-
-// const fs=require('fs')
-// console.time();
-// // const text=fs.readFileSync("demo.txt",'utf-8')// for sync read node is server so we want to continoues run
-// fs.readFile("demo.txt",'utf-8',(err,txt)=>{
-//     console.log(txt)
-// });
-// console.timeEnd();
-// // console.log(text)
-// console.log(lib.diff(25,5))
-
-// const express=require("express")
-// const server=express()
-// server.listen(8080);
-
-// console.log("hello world")
-// console.log("hello world 2")
-// console.log("hello world 3")
-
-const http = require("http");
 const fs = require("fs");
 
 const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
 const html = fs.readFileSync("index.html", "utf-8");
 const products = data.products;
 
+const express = require("express");
+const morgan=require('morgan');
 
-const server = http.createServer((req, res) => {
- 
-    
 
- if (req.url.startsWith('/product')) {
-    console.log(req.url , req.method)
-    const id=req.url.split('').pop();
-    const product=products.find(p=>p.id===(+id))
-    //  console.log(id)
-    res.setHeader('Content-Type','text/html');
-    let modifiedHtml=html.replace('**url**',product.thumbnail);
-    res.end(modifiedHtml)
-    return;
- }
-    
-  console.log("Server started");
-  switch (req.url) {
-    case "/":
-      res.setHeader("Content-Type", "text/html");
-      res.end(html);
-      break;
-    case "/api":
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify(data));
-      break;
+const server = express();
 
-    default:
-      res.writeHead(404, "Not Found");
-      res.end();
-      break;
-  }
+// server.use((req, res, next) => {
+//  console.log(
+//     req.method,
+//     req.ip,
+//     req.hostname,
+//     new Date(),
+//     req.get("User-Agent")
+//  );
+//   next();
+// });
+
+// server.use(morgan('default'));
+
+//bodyparser read from body json data
+server.use(express.json());
+//Server.use(express.urlencoded());
+
+//Static Hosting
+server.use(express.static('public'));
+
+// const auth = (req, res, next) => {
+//   // if (req.body.password === "123") {
+//   //   next();
+//   // } else {
+//   //   res.sendStatus(401);
+//   // }
+//   next();
+// };
+
+// API-Endpoints-Routes
+// API ROOT ,Base URL,example google.com/api/v2/
+
+
+// READ GET /products  perform C R U D Operations
+server.get("/products",(req,res)=>{
+  res.json(products);
+})
+
+// READ GET One /product
+server.get("/product/:id",  (req, res) => {
+  console.log(req.params.id);
+  const id=+req.params.id;
+  const product=products.find(p=>p.id===id);
+  res.json(product)
+  // res.send("hello world");
+  // res.sendFile('C:/Users/f3558/OneDrive/Desktop/React/Node-app/index.html')
+  // res.json(data)
+  // res.sendStatus(404)
+  // res.status(201).send('<h1>Hello world</h1>')
+  // res.json({ type: "GET 1" });
+}); //Always run top Code then Server Top when Api hits
+
+
+
+server.post("/products",  (req, res) => {
+  console.log(req.body);
+  products.push(req.body);
+  res.json(req.body);
 });
 
-server.listen(8080);
+server.put("/products/:id",  (req, res) => {
+  const id =+req.params.id;
+  const productIndex=products.findIndex(p=>p.id===id);
+ const modifiedProduct= products.splice(productIndex,1,{...req.body,id:id})
+
+  res.json(modifiedProduct);
+});
+server.patch("/products/:id",  (req, res) => {
+  const id =+req.params.id;
+  const productIndex=products.findIndex(p=>p.id===id);
+  const product=products[productIndex];
+  const modifiedProduct= products.splice(productIndex,1,{...product,...req.body})
+  
+  res.status(201).json(modifiedProduct);
+});
+
+server.delete("/products/:id",  (req, res) => {
+  const id =+req.params.id;
+  const productIndex=products.findIndex(p=>p.id===id);
+ const modifiedProduct= products.splice(productIndex,1)
+
+  res.status(201).json(modifiedProduct);
+});
+
+
+
+
+
+server.listen(8080, () => {
+  console.log("Server Start");
+});
