a2f5787bf1ced1384f9a2b710ef557a6eeb1f74d
Shahroz Ashraf khan <mr.sherry.793@gmail.com>
2024-04-03 23:25:44 +0500

    connect local database by using mongoose

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 
 
+require('dotenv').config()
 const express = require("express");
 const server = express();
 const productRouter=require('./routes/product')
 const userRouter=require('./routes/user')
+const mongoose = require('mongoose');
 
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce');
+   console.log('Database connected');
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 
 //bodyparser read from body json data
 server.use(express.json());
 //Static Hosting
 server.use(express.static("public"));
-
 server.use('/products',productRouter.router);
 server.use('/users',userRouter.router);
+
+
 
 
 server.listen(8080, () => {
